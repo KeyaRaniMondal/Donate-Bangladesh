c385b37f47d03298c2bc77c325baa3a86148cb7b
Sudip Kumar <keya24gadu2020@gmail.com>
2024-09-23 02:15:54 +0600

    connected homepage and blog page

---
+++ blog.js
@@ -0,0 +1,4 @@
+document.getElementById('home').addEventListener('click',function(){
+    if(document.getElementById('home').id==='home')
+        window.location.href='index.html'
+})
---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById('bloG').addEventListener('click',function(){
+    if(document.getElementById('bloG').id==='bloG')
+        window.location.href='blog.html'
+})
+
+
