d006c3c890f48d72874fe3eb41e631b0e0178f03
Sudip Kumar <keya24gadu2020@gmail.com>
2024-09-23 20:21:35 +0600

    added functionality to donation buttons and updated amounts

--- script.js
+++ script.js
@@ -1,6 +1,60 @@
-document.getElementById('bloG').addEventListener('click',function(){
-    if(document.getElementById('bloG').id==='bloG')
-        window.location.href='blog.html'
+// Home to Blog and Blog to Home page transaction
+
+document.getElementById('bloG').addEventListener('click', function () {
+    if (document.getElementById('bloG').id === 'bloG')
+        window.location.href = 'blog.html'
 })
+
+
+// Donate for Flood at Noakhali
+document.getElementById('btn-donate').addEventListener('click', function (event) {
+    event.preventDefault();
+
+    // donated amount
+    getDonationByID('donation');
+
+    // total amount
+    getTotalAmountByID('amount');
+
+    // check valid number and donate money
+    checkValidity('amount','newdonate');
+
+})
+
+
+
+// Donate for Flood at Feni
+document.getElementById('btn-donate1').addEventListener('click', function (event) {
+    event.preventDefault();
+
+    // donated amount
+    getDonationByID('donation1');
+
+    // total amount
+    getTotalAmountByID('amount');
+
+    // check valid number and donate money
+    checkValidity('amount','newdonate1');
+
+})
+
+
+
+// Donate for Aid fir quota movement
+document.getElementById('btn-donate2').addEventListener('click', function (event) {
+    event.preventDefault();
+
+    // donated amount
+    getDonationByID('donation2');
+
+    // total amount
+    getTotalAmountByID('amount');
+
+    // check valid number and donate money
+    checkValidity('amount','newdonate2');
+
+})
+
+
 
 
---
+++ utilities.js
@@ -0,0 +1,35 @@
+
+let d, a;
+
+//Made function to recieve donation amount
+
+function getDonationByID(id) {
+    d = parseFloat(document.getElementById(id).value);
+    return d;
+}
+
+
+//Made function to get the updated total amount after donation
+
+function getTotalAmountByID(id) {
+    a = parseFloat(document.getElementById('amount').innerText);
+    return a;
+}
+
+
+// created function to check valid number and donate money
+function checkValidity(amount, newdonate) {
+    if (isNaN(d) || d <= 0 || d > a)
+        alert('Invalid number of money can not be donated.Please enter positive number of money!!')
+    else {
+        const newAmount = a - d;
+        console.log(newAmount)
+        document.getElementById(amount).innerText = newAmount.toFixed(2)
+
+        const old_donation = parseFloat(document.getElementById(newdonate).innerText);
+        const totalDonation = old_donation + d;
+        console.log(totalDonation)
+        document.getElementById(newdonate).innerText = totalDonation.toFixed(2)
+
+    }
+}
